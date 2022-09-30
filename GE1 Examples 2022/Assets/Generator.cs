7314baed790c2de6b631a5d27a456461dc9b75f1
justinw1107 <justinwan1107@gmail.com>
2022-09-30 17:55:14 +0100

    created first ring of shapes

--- GE1 Examples 2022/Assets/Generator.cs
+++ GE1 Examples 2022/Assets/Generator.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    public int loops = 10;
+    public int loops = 6;
     public GameObject prefab;
     // Start is called before the first frame update
     void Start()
     {
+        int radius = 1;
+        for (int k=0;k<loops;k++)
+        {
+            int prefabno = (int)(2 * Mathf.PI * radius * k);
+            float theta = 2.0f*Mathf.PI/(float)prefabno;
+            for (int i=0;i<prefabno;i++)
+            {
+                Instantiate(prefab, new Vector3(Mathf.Sin(k*radius), Mathf.Cos(k*radius), 0), Quaternion.identity);
+            }
+        }
+        
     }
 
     // Update is called once per frame
@@ -17,3 +28,10 @@ public class Generator : MonoBehaviour
 
     }
 }
+
+/*
+NOTES
+
+EACH RING IS INCREMENTED BY 6 (6,12,18,24)
+
+*/
