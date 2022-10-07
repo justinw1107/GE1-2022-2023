cfda6ca00ec14653e7430c738c67d503246098bb
justinw1107 <justinwan1107@gmail.com>
2022-10-07 17:09:38 +0100

    first initial attempt

---
+++ GE1 Examples 2022/Assets/Snake.cs
@@ -0,0 +1,30 @@
+ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public Transform head;
+
+public Transform tail;
+
+public float frequency;
+
+public int headAmplitude;
+
+public int tailAmplitude;
+
+public float theta;
+
+public class Snake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
