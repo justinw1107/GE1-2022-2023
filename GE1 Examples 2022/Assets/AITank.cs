5274013b323e9122b11d3aa14e9a4da2fdf45846
justinw1107 <justinwan1107@gmail.com>
2022-10-20 21:35:29 +0100

    finished tasks

--- GE1 Examples 2022/Assets/AITank.cs
+++ GE1 Examples 2022/Assets/AITank.cs
@@ -1,4 +1,4 @@
-﻿    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -24,7 +24,7 @@ public class AITank : MonoBehaviour {
             // Gizmos.DrawWireSphere(pos, 1);
 
             float theta = (Mathf.PI * 2.0f) / numWaypoints;
-            for (int i = 0; i < numWaypoints; i++)
+            for (int i=0;i<numWaypoints;i++)
             {
                 float theta2 = i * theta;
                 Vector3 pos = new Vector3(Mathf.Sin(theta2) * radius, 0, Mathf.Cos(theta2) * radius);
@@ -41,7 +41,7 @@ public class AITank : MonoBehaviour {
         // Put code here to calculate the waypoints in a loop and 
         // Add them to the waypoints List
 
-        float theta = (Math.PI * 2.0f) / numWaypoints;
+        float theta = (Mathf.PI * 2.0f) / numWaypoints;
         for (int i=0;i<numWaypoints;i++)
         {
             float theta2 = i * theta;
@@ -57,13 +57,41 @@ public class AITank : MonoBehaviour {
         // Task 3
         // Put code here to move the tank towards the next waypoint
         // When the tank reaches a waypoint you should advance to the next one
+        Vector3 moveTarget = waypoints[current] - transform.position;
+        if (moveTarget.magnitude < 1)
+        {
+            current = (current + 1) % waypoints.Count;
+        }
 
+        moveTarget.Normalize();
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(moveTarget), Time.deltaTime*5);
+        transform.Translate(moveTarget*speed*Time.deltaTime, Space.World);
 
         // Task 4
-        // Put code here to check if the player is in front of or behindd the tank
+        // Put code here to check if the player is in front of or behind the tank
+        Vector3 movePlayer = player.position - transform.position;
+        if (Vector3.Dot(transform.forward, movePlayer)<0)
+        {
+            GameManager.Log("Player is behind the tank");
+        }
+        else
+        {
+            GameManager.Log("Player is in front of the tank");
+        }
+
         // Task 5
         // Put code here to calculate if the player is inside the field of view and in range
         // You can print stuff to the screen using:
-        GameManager.Log("Hello from th AI tank");
+        //GameManager.Log("Hello from th AI tank");
+        float fov = Mathf.Acos(Vector3.Dot(transform.forward, movePlayer)/movePlayer.magnitude)*Mathf.Rad2Deg;
+        if (fov < 45)
+        {
+            GameManager.Log("Player is inside the field of view of tank");
+        }
+        else
+        {
+            GameManager.Log("Player is outside the field of view of tank");
+        }
+
     }
 }
